2b42708f5df01c60aa94118db49ee0716563e6c2
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2023-09-21 00:25:02 +0300

    Updated fitness_center.py
    Reformated the program.

--- fitness_center.py
+++ fitness_center.py
@@ -1,28 +1,28 @@
-number_visitors=int(input())
-back=0
-chest=0
-legs=0
-abs_training=0
-protein_shake=0
-protein_bar=0
-work_out_percent=0
-protein_percent=0
+number_visitors = int(input())
+back = 0
+chest = 0
+legs = 0
+abs_training = 0
+protein_shake = 0
+protein_bar = 0
+work_out_percent = 0
+protein_percent = 0
 for training in range(number_visitors):
-    what_to_do_today=input()
-    if what_to_do_today=="Back":
-        back+=1
-    elif what_to_do_today=="Chest":
-        chest+=1
-    elif what_to_do_today=="Legs":
-        legs+=1
-    elif what_to_do_today=="Abs":
-        abs_training+=1
-    elif what_to_do_today=="Protein shake":
-        protein_shake+=1
+    what_to_do_today = input()
+    if what_to_do_today == "Back":
+        back += 1
+    elif what_to_do_today == "Chest":
+        chest += 1
+    elif what_to_do_today == "Legs":
+        legs += 1
+    elif what_to_do_today == "Abs":
+        abs_training += 1
+    elif what_to_do_today == "Protein shake":
+        protein_shake += 1
     else:
-        protein_bar+=1
-work_out_percent=(back+chest+legs+abs_training)/number_visitors*100
-protein_percent=(protein_shake+protein_bar)/number_visitors*100
+        protein_bar += 1
+work_out_percent = (back + chest + legs + abs_training) / number_visitors * 100
+protein_percent = (protein_shake + protein_bar) / number_visitors * 100
 print(f"{back} - back")
 print(f"{chest} - chest")
 print(f"{legs} - legs")
